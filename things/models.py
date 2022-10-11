2f327710dbe264a5dcfc5278b6a483b6342f9cfa
runxin.shao <runxin.shao@kcl.ac.uk>
2022-10-11 16:50:26 +0100

    fix some error

--- things/models.py
+++ things/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from django.core.validators import (MaxValueValidator,MinValueValidator,)
+
 # Create your models here.
-class Thing():
+class Thing(models.Model):
     name = models.TextField(unique = True,blank = False, max_length = 30)
     description = models.TextField(unique = False,blank = True, max_length = 120)
     quantity = models.IntegerField(unique = False,validators=[MinValueValidator(
-    0,message = 'can not less than 0')]
-    validators=[MaxValueValidators(100,message = 'can not bigger than 100')]
+    0,message = 'can not less than 0'),
+    MaxValueValidators(100,message = 'can not bigger than 100')]
     )
