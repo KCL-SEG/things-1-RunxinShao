d38815352348495ba212ba875a7f3747392cc919
runxin.shao <runxin.shao@kcl.ac.uk>
2022-10-11 13:38:52 +0100

    add a thing model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Thing():
+    name = models.TextField(unique = True,blank = False, max_length = 30)
+    description = models.TextField(unique = False,blank = True, max_length = 120)
+    quantity = models.IntegerField(unique = False,validators=[MinValueValidator(
+    0,message = 'can not less than 0')]
+    validators=[MaxValueValidators(100,message = 'can not bigger than 100')]
+    )
